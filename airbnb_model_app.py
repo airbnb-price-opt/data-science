c8ce42715a11da200d284c95efd400982dcfc801
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-08-01 13:50:47 -0400

    Adds more

--- airbnb_model_app.py
+++ airbnb_model_app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/api', methods=['POST'])
 def make_predict():
     # get data from post (4 features)
-    # data = request.get_json(force=True)
+    data = request.get_json(force=True)
     # # transforms
 
     # #
@@ -28,8 +28,9 @@ def make_predict():
     # # send preds back
     # output = {'y_hat': int(y_hat[0])}
     # return jsonify(results=output)
-    
-    return jsonify(request)
+    print("H E R E")
+    print(data)
+    return jsonify(results=data)
 
 if __name__ == '__main__':
     app.run(port=9000, debug=True)
